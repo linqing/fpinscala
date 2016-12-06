24682bc5180f5aaefeb4c081ce33ba4f8c4100bd
linqing <linqing01@gmail.com>
2016-12-06 23:14:22 +0800

    补充第二章answers注释

--- answers/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ answers/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -21,7 +21,7 @@ object MyModule {
     @annotation.tailrec
     def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
-      else go(n-1, n*acc)
+      else go(n - 1, n * acc)
 
     go(n, 1)
   }
@@ -30,20 +30,29 @@ object MyModule {
   def factorial2(n: Int): Int = {
     var acc = 1
     var i = n
-    while (i > 0) { acc *= i; i -= 1 }
+    while (i > 0) {
+      acc *= i;
+      i -= 1
+    }
     acc
   }
 
-  // Exercise 1: Write a function to compute the nth fibonacci number
-
-  // 0 and 1 are the first two numbers in the sequence,
-  // so we start the accumulators with those.
-  // At every iteration, we add the two numbers to get the next one.
+  /** 练习 2.1
+    * Exercise 1: Write a function to compute the nth fibonacci number
+    *
+    * 0 and 1 are the first two numbers in the sequence,
+    * so we start the accumulators with those.
+    * At every iteration, we add the two numbers to get the next one.
+    * 写一个尾递归实现的菲波拉契函数。
+    * f: 0  1  2  3  4  5  6 ...
+    * 0  1  1  2  3  5  8 ...
+    */
   def fib(n: Int): Int = {
     @annotation.tailrec
     def loop(n: Int, prev: Int, cur: Int): Int =
       if (n == 0) prev
       else loop(n - 1, cur, prev + cur)
+
     loop(n, 0, 1)
   }
 
@@ -71,6 +80,7 @@ object FormatAbsAndFactorial {
 // convenient to have syntax for constructing a function
 // *without* having to give it a name
 object AnonymousFunctions {
+
   import MyModule._
 
   // Some examples of anonymous functions:
@@ -81,7 +91,10 @@ object AnonymousFunctions {
     println(formatResult("increment2", 7, (x) => x + 1))
     println(formatResult("increment3", 7, x => x + 1))
     println(formatResult("increment4", 7, _ + 1))
-    println(formatResult("increment5", 7, x => { val r = x + 1; r }))
+    println(formatResult("increment5", 7, x => {
+      val r = x + 1;
+      r
+    }))
   }
 }
 
@@ -92,8 +105,8 @@ object MonomorphicBinarySearch {
   def findFirst(ss: Array[String], key: String): Int = {
     @annotation.tailrec
     def loop(n: Int): Int =
-      // If `n` is past the end of the array, return `-1`
-      // indicating the key doesn't exist in the array.
+    // If `n` is past the end of the array, return `-1`
+    // indicating the key doesn't exist in the array.
       if (n >= ss.length) -1
       // `ss(n)` extracts the n'th element of the array `ss`.
       // If the element at `n` is equal to the key, return `n`
@@ -126,14 +139,17 @@ object PolymorphicFunctions {
   }
 
 
-  // Exercise 2: Implement a polymorphic function to check whether
-  // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+  /** 练习 2.2
+    * Exercise 2: Implement a polymorphic function to check whether
+    * an `Array[A]` is sorted
+    * 写一个多态，尾递归的函数。
+    */
+  def isSorted[A](as: Array[A], gt: (A, A) => Boolean): Boolean = {
     @annotation.tailrec
     def go(n: Int): Boolean =
-      if (n >= as.length-1) true
-      else if (gt(as(n), as(n+1))) false
-      else go(n+1)
+      if (n >= as.length - 1) true
+      else if (gt(as(n), as(n + 1))) false
+      else go(n + 1)
 
     go(0)
   }
@@ -141,20 +157,24 @@ object PolymorphicFunctions {
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
 
-  def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
     (b: B) => f(a, b)
 
-  // Exercise 3: Implement `curry`.
-
-  // Note that `=>` associates to the right, so we could
-  // write the return type as `A => B => C`
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) =
+  /** 练习 2.3
+    * Exercise 3: Implement `curry`.
+    *
+    * Note that `=>` associates to the right, so we could
+    * write the return type as `A => B => C`
+    */
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
     a => b => f(a, b)
 
-  // NB: The `Function2` trait has a `curried` method already
-
-  // Exercise 4: Implement `uncurry`
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+  /** 练习 2.4
+    * NB: The `Function2` trait has a `curried` method already
+    * *
+    * Exercise 4: Implement `uncurry`
+    */
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
     (a, b) => f(a)(b)
 
   /*
@@ -166,9 +186,9 @@ object PolymorphicFunctions {
   we say that they are _isomorphic_ ("iso" = same; "morphe" = shape, form),
   a term we inherit from category theory.
   */
-
-  // Exercise 5: Implement `compose`
-
-  def compose[A,B,C](f: B => C, g: A => B): A => C =
+  /** 练习 2.5
+    * Exercise 5: Implement `compose`
+    */
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
     a => f(g(a))
 }
