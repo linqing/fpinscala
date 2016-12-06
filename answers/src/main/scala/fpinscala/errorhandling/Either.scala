1b90dcc3f02e642f348809f417f2a57a13171c67
linqing <linqing01@gmail.com>
2016-12-06 21:28:19 +0800

    注释第四章answers代码

--- answers/src/main/scala/fpinscala/errorhandling/Either.scala
+++ answers/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -2,72 +2,102 @@ package fpinscala.errorhandling
 
 
 //hide std library `Option` and `Either`, since we are writing our own in this chapter
-import scala.{Option => _, Either => _, _}
+import scala.{Option => _, Either => _}
 
-sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = 
-   this match {
-     case Right(a) => Right(f(a))
-     case Left(e) => Left(e)
-   }
-   
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
-   this match {
-     case Left(e) => Left(e)
-     case Right(a) => f(a)
-   }
- def orElse[EE >: E, AA >: A](b: => Either[EE, AA]): Either[EE, AA] =
-   this match {
-     case Left(_) => b
-     case Right(a) => Right(a)
-   }
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): 
-   Either[EE, C] = for { a <- this; b1 <- b } yield f(a,b1)
+sealed trait Either[+E, +A] {
+  /** 练习： 4.6 1/4
+    * 实现 map
+    */
+  def map[B](f: A => B): Either[E, B] =
+    this match {
+      case Right(a) => Right(f(a))
+      case Left(e) => Left(e)
+    }
+
+  /** 练习： 4.6 2/4
+    * 实现 flatMap
+    */
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+    this match {
+      case Left(e) => Left(e)
+      case Right(a) => f(a)
+    }
+
+  /** 练习： 4.6 3/4
+    * 实现 orElse
+    */
+  def orElse[EE >: E, AA >: A](b: => Either[EE, AA]): Either[EE, AA] =
+    this match {
+      case Left(_) => b
+      case Right(a) => Right(a)
+    }
+
+  /** 练习： 4.6 4/4
+    * 实现 map2
+    */
+  def map2[EE >: E, B, C](that: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {a <- this; b <- that} yield f(a, b)
 }
-case class Left[+E](get: E) extends Either[E,Nothing]
-case class Right[+A](get: A) extends Either[Nothing,A]
+case class Left[+E](get: E) extends Either[E, Nothing]
+case class Right[+A](get: A) extends Either[Nothing, A]
 
 object Either {
-  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
-    if (xs.isEmpty) 
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
       Left("mean of empty list!")
-    else 
+    else
       Right(xs.sum / xs.length)
 
-  def safeDiv(x: Int, y: Int): Either[Exception, Int] = 
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
     try Right(x / y)
-    catch { case e: Exception => Left(e) }
+    catch {
+      case e: Exception => Left(e)
+    }
 
   def Try[A](a: => A): Either[Exception, A] =
     try Right(a)
-    catch { case e: Exception => Left(e) }
+    catch {
+      case e: Exception => Left(e)
+    }
 
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = 
+  /** 练习 4.7 1/2
+    * 实现traverse
+    * 第一回合
+    */
+  def traverse[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
     es match {
       case Nil => Right(Nil)
-      case h::t => (f(h) map2 traverse(t)(f))(_ :: _)
+      case h :: t => (f(h) map2 traverse(t)(f)) (_ :: _)
     }
-  
-  def traverse_1[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = 
-    es.foldRight[Either[E,List[B]]](Right(Nil))((a, b) => f(a).map2(b)(_ :: _))
-  
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = 
+
+  /** 练习 4.7 1/2
+    * 实现traverse
+    * 第二回合
+    */
+  def traverse_1[E, A, B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es.foldRight[Either[E, List[B]]](Right(Nil))((a, b) => f(a).map2(b)(_ :: _))
+
+  /** 练习 4.7 2/2
+    * 实现sequence, 一根杆子插过去，返回第一个Error。
+    */
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
     traverse(es)(x => x)
 
-  /*
-  There are a number of variations on `Option` and `Either`. If we want to accumulate multiple errors, a simple
-  approach is a new data type that lets us keep a list of errors in the data constructor that represents failures:
-  
-  trait Partial[+A,+B]
-  case class Errors[+A](get: Seq[A]) extends Partial[A,Nothing]
-  case class Success[+B](get: B) extends Partial[Nothing,B]
-  
-  There is a type very similar to this called `Validation` in the Scalaz library. You can implement `map`, `map2`,
-  `sequence`, and so on for this type in such a way that errors are accumulated when possible (`flatMap` is unable to
-  accumulate errors--can you see why?). This idea can even be generalized further--we don't need to accumulate failing
-  values into a list; we can accumulate values using any user-supplied binary function.
-  
-  It's also possible to use `Either[List[E],_]` directly to accumulate errors, using different implementations of
-  helper functions like `map2` and `sequence`.
-  */
+  /** 练习 4.8
+    * 设计并实现一个可以处理多个错误的数据结构。
+    * There are a number of variations on `Option` and `Either`. If we want to accumulate multiple errors, a simple
+    * approach is a new data type that lets us keep a list of errors in the data constructor that represents failures:
+    * *
+    * trait Partial[+A,+B]
+    * case class Errors[+A](get: Seq[A]) extends Partial[A,Nothing]
+    * case class Success[+B](get: B) extends Partial[Nothing,B]
+    * *
+    * There is a type very similar to this called `Validation` in the Scalaz library. You can implement `map`, `map2`,
+    * `sequence`, and so on for this type in such a way that errors are accumulated when possible (`flatMap` is unable to
+    * accumulate errors--can you see why?). This idea can even be generalized further--we don't need to accumulate failing
+    * values into a list; we can accumulate values using any user-supplied binary function.
+    * *
+    * It's also possible to use `Either[List[E],_]` directly to accumulate errors, using different implementations of
+    * helper functions like `map2` and `sequence`.
+    */
 }
--- answers/src/main/scala/fpinscala/errorhandling/Option.scala
+++ answers/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,48 +1,75 @@
 package fpinscala.errorhandling
 
 //hide std library `Option` and `Either`, since we are writing our own in this chapter
-import scala.{Option => _, Either => _, _}
+import scala.{Option => _, Either => _}
 
 sealed trait Option[+A] {
+  /** 练习 4.1 1/5
+    * 实现map
+    */
   def map[B](f: A => B): Option[B] = this match {
     case None => None
     case Some(a) => Some(f(a))
   }
 
-  def getOrElse[B>:A](default: => B): B = this match {
+  /** 练习 4.1 2/5
+    * 实现getOrElse
+    */
+  def getOrElse[B >: A](default: => B): B = this match {
     case None => default
     case Some(a) => a
   }
 
+  /** 练习 4.1 3/5
+    * 实现flatMap
+    * 第一版： 使用map和getOrElse实现。
+    */
   def flatMap[B](f: A => Option[B]): Option[B] =
     map(f) getOrElse None
 
-  /*
-  Of course, we can also implement `flatMap` with explicit pattern matching.
-  */
+  /** 练习 4.1 3/5
+    * 实现flatMap
+    *
+    * Of course, we can also implement `flatMap` with explicit pattern matching.
+    * 第二版： 使用Pattern Matching实现。
+    */
   def flatMap_1[B](f: A => Option[B]): Option[B] = this match {
     case None => None
     case Some(a) => f(a)
   }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] =
+
+  /** 练习 4.1 4/5
+    * 实现orElse
+    * 第一版： 用map实现。
+    */
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
     this map (Some(_)) getOrElse ob
 
-  /*
-  Again, we can implement this with explicit pattern matching.
-  */
-  def orElse_1[B>:A](ob: => Option[B]): Option[B] = this match {
+  /** 练习 4.1 2/5
+    * 实现orElse
+    * Again, we can implement this with explicit pattern matching.
+    * 第二版：显式Pattern matching实现
+    */
+  def orElse_1[B >: A](ob: => Option[B]): Option[B] = this match {
     case None => ob
     case _ => this
   }
 
+  /** 练习 4.1 4/5
+    * 实现filter
+    * 第一版： 用pattern matching实现。
+    */
   def filter(f: A => Boolean): Option[A] = this match {
     case Some(a) if f(a) => this
     case _ => None
   }
-  /*
-  This can also be defined in terms of `flatMap`.
-  */
+
+  /** 练习 4.1 4/5
+    * 实现filter
+    * This can also be defined in terms of `flatMap`.
+    * 第二版： 用flatMap实现。
+    */
   def filter_1(f: A => Boolean): Option[A] =
     flatMap(a => if (f(a)) Some(a) else None)
 }
@@ -59,7 +86,9 @@ object Option {
     }
     // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern
     // that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
-    catch { case _: Exception => 43 }
+    catch {
+      case _: Exception => 43
+    }
   }
 
   def failingFn2(i: Int): Int = {
@@ -68,46 +97,74 @@ object Option {
       // A thrown Exception can be given any type; here we're annotating it with the type `Int`
       x + ((throw new Exception("fail!")): Int)
     }
-    catch { case _: Exception => 43 }
+    catch {
+      case _: Exception => 43
+    }
   }
 
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
 
+  /** 练习： 4.2
+    * 用flatMap实现求方差函数variance
+    * https://en.wikipedia.org/wiki/Variance
+    * 用flatMap处理： 撸函数嵌套调用撸函数。
+    */
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  // a bit later in the chapter we'll learn nicer syntax for
-  // writing functions like this
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+  /** 练习： 4.3
+    * // a bit later in the chapter we'll learn nicer syntax for
+    * // writing functions like this
+    * 实现map2函数： 两个都就绪了才能撸，差一个都撸不成。
+    */
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     a flatMap (aa => b map (bb => f(aa, bb)))
 
-  /*
-  Here's an explicit recursive version:
-  */
+  /** 练习：4.4
+    * Here's an explicit recursive version:
+    * 实现sequence函数。一串撸=>撸一串, 遇到任何一个Nil，整个结果就是Nil。
+    * 第一版： 用Pattern Matching实现。
+    */
   def sequence[A](a: List[Option[A]]): Option[List[A]] =
     a match {
       case Nil => Some(Nil)
       case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
     }
-  /*
-  It can also be implemented using `foldRight` and `map2`. The type annotation on `foldRight` is needed here; otherwise
-  Scala wrongly infers the result type of the fold as `Some[Nil.type]` and reports a type error (try it!). This is an
-  unfortunate consequence of Scala using subtyping to encode algebraic data types.
-  */
-  def sequence_1[A](a: List[Option[A]]): Option[List[A]] =
-    a.foldRight[Option[List[A]]](Some(Nil))((x,y) => map2(x,y)(_ :: _))
 
+  /** 练习：4.4
+    * It can also be implemented using `foldRight` and `map2`. The type annotation on `foldRight` is needed here; otherwise
+    * Scala wrongly infers the result type of the fold as `Some[Nil.type]` and reports a type error (try it!). This is an
+    * unfortunate consequence of Scala using subtyping to encode algebraic data types.
+    * 实现sequence函数。
+    * 第二版： 用foldRight实现。
+    */
+  def sequence_1[A](a: List[Option[A]]): Option[List[A]] =
+    a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x, y)(_ :: _))
+
+  /** 练习： 4.5
+    * 实现traverse： 用一个可能走不通的函数，在一个串里面走啊走，走到走不通了就返回。
+    * 返回已经走过的列表结果。
+    * 第一回合： 用pattern match加map2实现。
+    */
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
     a match {
       case Nil => Some(Nil)
-      case h::t => map2(f(h), traverse(t)(f))(_ :: _)
+      case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
     }
 
+  /** 练习： 4.5
+    * 实现traverse： 用一个可能走不通的函数，在一个串里面走啊走，走到走不通了就返回。
+    * 返回已经走过的列表结果。
+    * 第二回合： 用pattern match加map2实现。
+    */
   def traverse_1[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
-    a.foldRight[Option[List[B]]](Some(Nil))((h,t) => map2(f(h),t)(_ :: _))
+    a.foldRight[Option[List[B]]](Some(Nil))((h, t) => map2(f(h), t)(_ :: _))
 
+  /** 练习： 4.5
+    * 第三回合： 用traverse来实现sequence。
+    */
   def sequenceViaTraverse[A](a: List[Option[A]]): Option[List[A]] =
     traverse(a)(x => x)
 }
